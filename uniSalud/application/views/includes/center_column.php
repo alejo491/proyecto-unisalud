2623ca5aedb20ddb3e5f804f6642c40f2a856c7c
waruano9212@gmail.com <waruano9212@gmail.com@28f0b55a-09cc-baf4-5b67-3efebd8d3a5b>
2013-08-26 13:54:48 +0000

    Organizacion de la plantilla

---
+++ uniSalud/application/views/includes/center_column.php
@@ -0,0 +1,13 @@
+<td class="boxtd">
+    <div class="featured">
+        <br/><br/><br/>
+        <span class="titleprod">Aspirine&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$9.99</span><br/><br/>
+        <a class="navlink" href="#"><img src="images/photos/m1.png" width="181px;" height="140px" alt="Click Here" border="0"/></a>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dolor nisi, lobortis laoreet malesuada a, pretium egpellentesque, est sem malesuaugue.</p>
+        <p class="btns"><a class="navlink" href="#"><img src="images/btns/addtocart.png" width="87px;" height="31px" alt="Click Here" border="0"/></a>&nbsp;&nbsp;<a class="navlink" href="#"><img src="images/btns/details.png" width="87px;" height="31px" alt="Click Here" border="0"/></a></p>
+        <span class="titleprod">Viagra&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$9.99</span><br/><br/>
+        <a class="navlink" href="#"><img src="images/photos/m2.png" width="181px;" height="140px" alt="Click Here" border="0"/></a>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dolor nisi, lobortis laoreet malesuada a, pretium egpellentesque, est sem malesuaugue.</p>
+        <p class="btns"><a class="navlink" href="#"><img src="images/btns/addtocart.png" width="87px;" height="31px" alt="Click Here" border="0"/></a>&nbsp;&nbsp;<a class="navlink" href="#"><img src="images/btns/details.png" width="87px;" height="31px" alt="Click Here" border="0"/></a></p>
+    </div>
+</td>
---
+++ uniSalud/application/views/plantilla.php
@@ -0,0 +1,9 @@
+<?php 
+/*Plantilla de la Pagina, Aqui se cargan todos los contenidos*/
+$this->load->view('includes/head');
+$this->load->view('includes/menu');
+$this->load->view('includes/left_column');
+$this->load->view('includes/center_column');
+$this->load->view('includes/right_column');
+$this->load->view('includes/footer');
+?>
