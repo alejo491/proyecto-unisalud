ff45183f454baa5b1b7a6c86a7ffb1024f2f5c8a
alejo491 <alejo491@28f0b55a-09cc-baf4-5b67-3efebd8d3a5b>
2013-09-01 05:12:05 +0000

    se creo el controlador medicos y la vista de registrar medico

---
+++ uniSalud/application/controllers/medicos.php
@@ -0,0 +1,21 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Medicos extends CI_Controller {
+
+    function _construct() {
+        parent::_construct();
+    }
+
+    public function index() {
+         $data['titulo'] = "Personal UniSalud";
+	 $data['title'] = 'Personal UniSalud';
+         $data['contenido'] = 'reegistro_medico';
+         $this->load->view('plantilla', $data);
+       
+    }
+
+    public function registrar(){}
+    
+}
---
+++ uniSalud/application/views/personal/registrar_medico.php
@@ -0,0 +1,190 @@
+<!--content -->
+<div class="body3">
+	<div class="main">
+		<?php $this->load->view("estandar/topcontent");?>
+	</div>
+</div>
+<div class="main">
+	
+		<div class="wrapper pad_bot1">
+			
+                       
+       
+                    <?php
+                    $this->load->helper('form');
+                    $atributos=array(
+                        
+                        'id'    => 'formRegistroPersonal'
+                    );
+                    echo form_open('medicos/registrar', $atributos);
+                    ?>
+                    <h1>Registro de Nuevo Usuario</h1><br />
+                    <table>
+                    <td><strong>Los campos marcados con (*) son obligatorios</strong></td><br />
+                        <tr>
+                            <td><strong>Nombres:*</strong></td>
+                            <td><?php
+                                $data_form=array(
+                                    'id'        => 'nombre',
+                                    'nombre'    =>  'nombre',
+                                    'size'      =>  '40',
+                                    'value'     => set_value('nombre')
+                                );
+                               echo form_error('nombre','<b><p style="color:red;">','</p></b>'); 
+                               echo form_input($data_form);
+                            
+                            
+                                ?></td>
+                         </tr>
+                         <tr>
+                             <td><strong>Apellidos:*</strong></td><td></td>
+                             <td><?php
+                                $data_form=array(
+                                    'id'        => 'apellido',
+                                    'nombre'    =>  'apellido',
+                                    'size'      =>  '40',
+                                    'value'     => set_value('apellido')
+                                );
+                               echo form_error('apellido','<b><p style="color:red;">','</p></b>'); 
+                               echo form_input($data_form);
+                            
+                            
+                                ?></td>
+                         </tr>
+                         <tr>
+                             <td><strong>Cedula:*</strong></td><td></td>
+                             <td><?php
+                                $data_form=array(
+                                    'id'        => 'cedula',
+                                    'nombre'    =>  'cedula',
+                                    'size'      =>  '40',
+                                    
+                                );
+                               echo form_error('cedula','<b><p style="color:red;">','</p></b>'); 
+                               echo form_input($data_form);
+                            
+                            
+                                ?></td>
+                         </tr>
+                         <tr>
+                             <td><strong>N° Tarjeta Profesional:*</strong></td><td></td>
+                             <td><?php
+                                $data_form=array(
+                                    'id'        => 'Tarjeta',
+                                    'nombre'    =>  'Tarjeta',
+                                    'size'      =>  '40',
+                                    
+                                );
+                               echo form_error('Tarjeta','<b><p style="color:red;">','</p></b>'); 
+                               echo form_input($data_form);
+                            
+                            
+                                ?></td>
+                         </tr>
+                         <tr>
+                             <td><strong>Profesion:*</strong></td><td></td>
+                             <td><?php
+                                $data_form=array(
+                                    'id'        => 'Profesion',
+                                    'nombre'    =>  'Profesion',
+                                    'size'      =>  '40',
+                                    
+                                );
+                               echo form_error('Profesion','<b><p style="color:red;">','</p></b>'); 
+                               echo form_input($data_form);
+                            
+                            
+                                ?></td>
+                         </tr>
+                         <tr>
+                             <td><strong>Especialidad:*</strong></td><td></td>
+                             <td><?php
+                                $data_form=array(
+                                    'id'        => 'especialidad',
+                                    'nombre'    =>  'especialidad',
+                                    'size'      =>  '40',
+                                    
+                                );
+                               echo form_error('especialidad','<b><p style="color:red;">','</p></b>'); 
+                               echo form_input($data_form);
+                            
+                            
+                                ?></td>
+                         </tr>
+                          <tr>
+                             <td><strong>E-mail:*</strong></td><td></td>
+                             <td><?php
+                                $data_form=array(
+                                    'id'        => 'email',
+                                    'nombre'    =>  'email',
+                                    'size'      =>  '40',
+                                    
+                                );
+                               echo form_error('email','<b><p style="color:red;">','</p></b>'); 
+                               echo form_input($data_form);
+                            
+                            
+                                ?></td>
+                         </tr>
+                         <tr>
+                             <td><strong>Comfirmar E-mail:*</strong></td><td></td>
+                             <td><?php
+                                $data_form=array(
+                                    'id'        => 'comemail',
+                                    'nombre'    =>  'comemail',
+                                    'size'      =>  '40',
+                                    
+                                );
+                               echo form_error('comfirmaremail','<b><p style="color:red;">','</p></b>'); 
+                               echo form_input($data_form);
+                            
+                            
+                                ?></td>
+                         </tr>
+                         <tr>
+                             <td><strong>Servicio a prestar:*</strong></td><td></td>
+                             <td><?php
+                                $data_form=array(
+                                    'id'        => 'servicio',
+                                    'nombre'    =>  'servicio',
+                                    'size'      =>  '40',
+                                    
+                                );
+                               echo form_error('servicio','<b><p style="color:red;">','</p></b>'); 
+                               echo form_input($data_form);
+                            
+                            
+                                ?></td>
+                         </tr>
+                           <tr>
+                             <td><strong>Servicio a prestar:*</strong></td><td></td>
+                             <td><?php
+                                $data_form=array(
+                                    'id'        => 'servicio',
+                                    'nombre'    =>  'servicio',
+                                    'size'      =>  '40',
+                                    
+                                );
+                               echo form_error('servicio','<b><p style="color:red;">','</p></b>'); 
+                               echo form_input($data_form);
+                            
+                            
+                                ?></td>
+                         </tr>
+                   
+                    </table><br />
+                    <br />
+                    <table id ="btnRegistro">
+                            <tr>
+                                <td><input  type="submit" value="Registrar" /></td>
+                                
+                             </tr>
+                    </table>
+ 
+		
+                   <?php
+                               echo form_close();
+                   ?>
+		</div>
+	
+</div>
