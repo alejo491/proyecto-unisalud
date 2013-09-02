ee0ed070d9a2efa9ac15a96dcab810f297e31053
waruano9212@gmail.com <waruano9212@gmail.com@28f0b55a-09cc-baf4-5b67-3efebd8d3a5b>
2013-09-02 03:15:20 +0000

    Configuracion de la base de datos

--- uniSalud/application/config/config.php
+++ uniSalud/application/config/config.php
@@ -1,5 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
+//
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'oaiwjefoijawef9823u498aofijwaef';
 
 /*
 |--------------------------------------------------------------------------
@@ -279,7 +279,7 @@ $config['cookie_secure']	= FALSE;
 | COOKIE data is encountered
 |
 */
-$config['global_xss_filtering'] = FALSE;
+$config['global_xss_filtering'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
