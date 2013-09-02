5d4b5b7950e511a31734bf73937b3d0d0a242e74
waruano9212@gmail.com <waruano9212@gmail.com@28f0b55a-09cc-baf4-5b67-3efebd8d3a5b>
2013-09-02 03:15:20 +0000

    Configuracion de la base de datos

--- trunk/uniSalud/application/models/agendaModelo.php
+++ trunk/uniSalud/application/models/agendaModelo.php
@@ -3,9 +3,13 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class agendaModelo extends CI_Model {       
+class agendaModelo extends CI_Model {
+    
         function __construct() {
         parent::__construct();
+    }
+    function getAgenda($id_medico){
+        
     }
 }
 ?>
