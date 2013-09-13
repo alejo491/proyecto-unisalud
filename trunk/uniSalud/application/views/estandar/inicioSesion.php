3bef29fc88f43869950a3548a6eee1be5f18f2f9
alejo491 <alejo491@28f0b55a-09cc-baf4-5b67-3efebd8d3a5b>
2013-09-13 00:18:40 +0000

    creadas vistas para registrar usuario,añadida la funcion index al controlador usuario y incluido el manejo de las variables de la plantilla

--- trunk/uniSalud/application/views/estandar/inicioSesion.php
+++ trunk/uniSalud/application/views/estandar/inicioSesion.php
@@ -3,7 +3,7 @@
                     
 			<table >
 			<tr><td align="left" style="color:white">E-mail:</td><td align="left" style="color:white">Contraseña:</td></tr>
-                        <tr><td><input class="text" name="email" id='email' size="15" maxlength="64"/></td><td><input type='password' class="text" name="contrasena" id='contrasena' size="15" maxlength="64" /></td><td><input value="Ingresar" type="submit" /></td><td><a  style="color:white">Registrarse</a></td></tr>
+                        <tr><td><input class="text" name="email" id='email' size="15" maxlength="64"/></td><td><input type='password' class="text" name="contrasena" id='contrasena' size="15" maxlength="64" /></td><td><input value="Ingresar" type="submit" /></td><td><a href=".<?php base_url()?>/usuario/index" style="color:white">Registrarse</a></td></tr>
 			</table>
         </form>
 </div>
---
+++ trunk/uniSalud/application/views/personal/topcontentRegistrarse.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "OLA OLA";
+?>
