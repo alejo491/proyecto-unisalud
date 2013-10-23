6de3c0180b827100641a678eb1b7c3f31d722c68
jazmin.dag <jazmin.dag@28f0b55a-09cc-baf4-5b67-3efebd8d3a5b>
2013-10-23 03:17:28 +0000

    El botón Consultorio del menú se comentó y se añadió a la base de datos un insert into para la tabla consultorio

--- trunk/uniSalud/application/views/personal/menu.php
+++ trunk/uniSalud/application/views/personal/menu.php
@@ -3,7 +3,7 @@
         <li id="menu_active"><?php echo anchor(base_url(), 'Inicio'); ?></li>
         <li id="menu_active"><?php echo anchor('personalSaludControlador/index', 'Personal Medico'); ?></li>
         <li id="menu_active"><?php echo anchor('programaSaludControlador/index', 'Programa de Salud'); ?></li>
-        <li id="menu_active"><?php echo anchor('consultorioControlador/index', 'Consultorios'); ?></li>
+        <!--<li id="menu_active"> <?php echo anchor('consultorioControlador/index', 'Consultorios'); ?></li> -->
         <li id="menu_active"><?php echo anchor('estandarControlador/index', 'Acerca de'); ?></li>
         
     </ul>
