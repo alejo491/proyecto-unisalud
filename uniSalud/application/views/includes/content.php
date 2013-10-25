0388c2566645aa4668285b8d072e63f234157c83
waruano9212@gmail.com <waruano9212@gmail.com@28f0b55a-09cc-baf4-5b67-3efebd8d3a5b>
2013-10-25 04:00:32 +0000

    modificado script base de datos... y cambiado el actualizar y volver por solo actualizar

--- uniSalud/application/views/includes/content.php
+++ uniSalud/application/views/includes/content.php
@@ -13,5 +13,13 @@
             $this->load->view($content);
         }
 ?>
+    <div id="mensaje">
+        <?php 
+            $session = $this->session->all_userdata();
+            if(isset($session['mensaje']) && $session['mensaje']!=NULL && $session['mensaje']!=""):
+        ?>
+        <h4 style="background: none; color: #480091; border: none; margin: 0px; font-size: 20px;"><?php echo $session['mensaje'];?></h4>
+        <?php endif;?>
+    </div>
 </div>
 <!--content end-->
--- uniSalud/application/views/personal/footerMenu.php
+++ uniSalud/application/views/personal/footerMenu.php
@@ -1,7 +1,11 @@
 <div id="footer_menu">
     <nav>
         <ul>
-            <li><a href="index.html">Personal Medico</a></li>
+         <li id="menu_active"><?php echo anchor(base_url(), 'Inicio'); ?></li>
+        <li id="menu_active"><?php echo anchor('personalSaludControlador/index', 'Personal Medico'); ?></li>
+        <li id="menu_active"><?php echo anchor('programaSaludControlador/index', 'Programa de Salud'); ?></li>
+        <!--<li id="menu_active"> <?php echo anchor('consultorioControlador/index', 'Consultorios'); ?></li> -->
+        <li id="menu_active"><?php echo anchor('estandarControlador/index', 'Acerca de'); ?></li>
         </ul>
     </nav>
 </div>
