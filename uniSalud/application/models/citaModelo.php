49bdaf5ef40bd55ed890a346e0c4cc037e421a56
alejo491 <alejo491@28f0b55a-09cc-baf4-5b67-3efebd8d3a5b>
2013-12-16 22:43:57 +0000

    agrego validacion en reservar cita

--- uniSalud/application/models/citaModelo.php
+++ uniSalud/application/models/citaModelo.php
@@ -11,11 +11,15 @@ class citaModelo extends CI_Model {
         return $this->db->insert('cita',$info_reserva);
     }
     
-    function tieneCita($id){
+    function tieneCita($id,$id2){
         
+        $where=array(
+         'id_estudiante'=>$id,
+         'id_personalsalud'=>$id2
+                );
         $this->db->limit(1);
         $this->db->select('id_estudiante');
-        $this->db->where('id_estudiante',$id);
+        $this->db->where($where);
         $this->db->from('cita');
         $consulta=$this->db->get();
         if($consulta->num_rows()>0){
@@ -24,6 +28,21 @@ class citaModelo extends CI_Model {
         else{
             return FALSE;
         }
+    }
+    
+    function verificarActividad($actividad){
+        
+        $sql = "SELECT * FROM CITA NATURAL JOIN PROGRAMASALUD WHERE ESTADO <>3 AND ESTADO <>4 AND ACTIVIDAD = '".$actividad."'";
+        $query = $this->db->query($sql);
+        
+        if ($query->num_rows() > 0) {
+            
+            return TRUE;
+        }
+        else{
+            return FALSE;
+        }
+        
     }
 }
 ?>
